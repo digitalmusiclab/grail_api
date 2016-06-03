681eadf29abb49ef743cc71b059d1a739f36a584
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2016-06-03 17:27:15 -0400

    Fix: Authentication now passes if not authentication password or token

--- api.py
+++ api.py
@@ -65,7 +65,6 @@ def verify_password(username_or_token, password):
     # first try to authenticate by token
     user = User.verify_auth_token(username_or_token)
     if not user:
-        return False
         # try to authenticate with username/password
         user = User.query.filter_by(username=username_or_token).first()
         if not user or not user.verify_password(password):
