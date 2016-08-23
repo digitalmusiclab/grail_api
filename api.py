fbb4df84ecaccba074be8dc2f54d1ff294d9933d
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2016-08-23 16:59:26 -0400

    Fix: Comment out API calls that do not match

--- api.py
+++ api.py
@@ -142,22 +142,22 @@ def grail_api():
         return jsonify(**q)
         # Using HTML template
         # return render_template('hello.html', name=q)
-    elif 'musicbrainz_artist' in request.args:
-        search = solr.search(q='musicbrainz_artist:' + request.args['musicbrainz_artist'])
-        q = search.raw_response
-        return jsonify(**q)
-    elif 'musixmatch_artist' in request.args:
-        search = solr.search(q='musixmatch_artist:' + request.args['musixmatch_artist'])
-        q = search.raw_response
-        return jsonify(**q)
-    elif 'spotify_artist' in request.args:
-        search = solr.search(q='spotify_artist:' + request.args['spotify_artist'])
-        q = search.raw_response
-        return jsonify(**q)
-    elif 'isrc' in request.args:
-        search = solr.search(q='isrc:' + request.args['isrc'])
-        q = search.raw_response
-        return jsonify(**q)
+    # elif 'musicbrainz_artist' in request.args:
+    #     search = solr.search(q='musicbrainz_artist:' + request.args['musicbrainz_artist'])
+    #     q = search.raw_response
+    #     return jsonify(**q)
+    # elif 'musixmatch_artist' in request.args:
+    #     search = solr.search(q='musixmatch_artist:' + request.args['musixmatch_artist'])
+    #     q = search.raw_response
+    #     return jsonify(**q)
+    # elif 'spotify_artist' in request.args:
+    #     search = solr.search(q='spotify_artist:' + request.args['spotify_artist'])
+    #     q = search.raw_response
+    #     return jsonify(**q)
+    # elif 'isrc' in request.args:
+    #     search = solr.search(q='isrc:' + request.args['isrc'])
+    #     q = search.raw_response
+    #     return jsonify(**q)
     else:
         return 'Enter your query'
 
